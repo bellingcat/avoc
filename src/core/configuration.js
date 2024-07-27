02133dd1c7986bba554950e1544e7157d8ada93d
haruspeks <fabio.daniele@protonmail.com>
2024-07-27 19:46:37 +0200

    chore(config): add JSDOC

--- src/core/configuration.js
+++ src/core/configuration.js
@@ -1,9 +1,18 @@
 class Configuration {
+    /**
+     * 
+     * @param {Object} module 
+     * @param {Db} db 
+     */
     constructor(module, db) {
         this.module = module;
         this.db = db;
     }
 
+    /**
+     * @async
+     * @returns {Object} 
+     */
     async load() {
         const fileSettings = this.module.config().configuration;
         const dbSettings = await this.db.get("Avoc.settings", {});
