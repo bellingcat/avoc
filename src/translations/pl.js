76ae9c1d7e471b26ad27705a6e7c62191b56f768
P3run <P3run@users.noreply.github.com>
2024-08-01 22:44:09 +0200

    Add Polish language

---
+++ src/translations/pl.js
@@ -0,0 +1,19 @@
+define("translations/pl", function() {
+    return new Language("pl", {
+        lockScreen: "Zablokuj Widok",
+        lockScreenTooltip: "Wyłącz synchronizację dla poszczególnych widoków",
+        weather: "Pogoda",
+        shortcuts: "Skróty",
+        elevation: "Elewacja",
+        temperature: "Temperatura",
+        precipitation: "Opady",
+        cloudCover: "Zachmurzenie",
+        windSpeed: "Prędkość wiatru",
+        windDirection: "Kierunek wiatru",
+        weatherLabel: "Etykieta Światowej Organizacji Meteorologicznej",
+        lastUpdate: "Ostatnia aktualizacja",
+        longitude: "Długość",
+        latitude: "Szerokość",
+        goTo: "Idź do",
+    });
+});
